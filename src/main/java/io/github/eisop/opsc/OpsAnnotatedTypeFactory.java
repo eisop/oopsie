55e9561b4e5b9cc2af4c2c3f1778ea945af87943
Thomas Kirz <thomas.kirz@gmail.com>
2023-07-10 17:59:35 +0200

    Annotate parameter ('in') types
    Works for postgres, nullability annotations etc. not supported yet

--- src/main/java/io/github/eisop/opsc/OpsAnnotatedTypeFactory.java
+++ src/main/java/io/github/eisop/opsc/OpsAnnotatedTypeFactory.java
@@ -4,12 +4,17 @@ import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.LiteralTree;
 import com.sun.source.tree.MethodInvocationTree;
 import io.github.eisop.opsc.db.SchemaInfo;
+import io.github.eisop.opsc.exception.OpsDatabaseException;
 import io.github.eisop.opsc.qual.Sql;
 import io.github.eisop.opsc.qual.SqlBottom;
 import io.github.eisop.opsc.qual.SqlUnknown;
 import java.lang.annotation.Annotation;
 import java.sql.SQLException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
 import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.util.Elements;
@@ -91,13 +96,6 @@ public class OpsAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
         return new OpsTransfer((CFAnalysis) analysis);
     }
 
-    /** Returns a new SQL annotation with the given output type. */
-    AnnotationMirror createSQLAnnotation(String[] out) {
-        AnnotationBuilder builder = new AnnotationBuilder(processingEnv, Sql.class);
-        builder.setValue("out", out);
-        return builder.build();
-    }
-
     @Override
     protected QualifierHierarchy createQualifierHierarchy() {
         return new OpsQualifierHierarchy(this.getSupportedTypeQualifiers(), elements);
@@ -119,10 +117,85 @@ public class OpsAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
                 AnnotationMirror superAnno,
                 QualifierKind superKind) {
             if (subKind == SQL_KIND && superKind == SQL_KIND) {
-                // TODO: Compare subAnno and superAnno.
-                return AnnotationUtils.areSame(subAnno, superAnno);
+                List<String> subIn =
+                        AnnotationUtils.getElementValueArray(
+                                subAnno, sqlInElement, String.class, Collections.emptyList());
+                List<String> subOut =
+                        AnnotationUtils.getElementValueArray(
+                                subAnno, sqlOutElement, String.class, Collections.emptyList());
+                List<String> superIn =
+                        AnnotationUtils.getElementValueArray(
+                                superAnno, sqlInElement, String.class, Collections.emptyList());
+                List<String> superOut =
+                        AnnotationUtils.getElementValueArray(
+                                superAnno, sqlOutElement, String.class, Collections.emptyList());
+
+                return outIsSubtype(subOut, superOut) && inIsSubtype(subIn, superIn);
             }
             throw new TypeSystemError("Unexpected qualifiers: %s %s", subAnno, superAnno);
+        }
+
+        private boolean outIsSubtype(List<String> subOut, List<String> superOut) {
+            // Compare lengths: Supertype can have more columns than subtype as it does not have to
+            // deal with all columns of the subtype
+            if (subOut.size() < superOut.size()) {
+                return false;
+            }
+            if (subOut.size() > superOut.size()) {
+                return outIsSubtype(subOut, subOut.subList(0, superOut.size()));
+            }
+
+            for (int i = 0; i < subOut.size(); i++) {
+                String[] sub = subOut.get(i).split(" ");
+                String[] sup = superOut.get(i).split(" ");
+                String subType = sub[sub.length - 1];
+                String supType = sup[sup.length - 1];
+
+                if (!subType.equals(supType)) {
+                    return false;
+                }
+
+                if (Arrays.asList(sup).contains("@NotNull")
+                        && !Arrays.asList(sub).contains("@NotNull")) {
+                    return false;
+                }
+
+                if (Arrays.stream(sup).anyMatch(s -> s.startsWith("@MaxLength("))) {
+                    if (Arrays.stream(sub).noneMatch(s -> s.startsWith("@MaxLength("))) {
+                        return false;
+                    }
+                    int superMax =
+                            Arrays.stream(sup)
+                                    .filter(s -> s.startsWith("@MaxLength("))
+                                    .map(
+                                            s ->
+                                                    Integer.parseInt(
+                                                            s.split("\\(", 2)[1]
+                                                                    .split("\\)", 2)[0]))
+                                    .findFirst()
+                                    .get();
+                    int subMax =
+                            Arrays.stream(sub)
+                                    .filter(s -> s.startsWith("@MaxLength("))
+                                    .map(
+                                            s ->
+                                                    Integer.parseInt(
+                                                            s.split("\\(", 2)[1]
+                                                                    .split("\\)", 2)[0]))
+                                    .findFirst()
+                                    .get();
+                    if (subMax > superMax) {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private boolean inIsSubtype(List<String> subIn, List<String> superIn) {
+            // Reverse of the hierarchy for out except that the lengths must be equal as parameters
+            // have to be specified and no more parameters than exist can be specified.
+            return subIn.size() == superIn.size() && outIsSubtype(superIn, subIn);
         }
 
         @Override
@@ -181,13 +254,20 @@ public class OpsAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
                 if (!type.hasAnnotationRelaxed(SQL)) {
                     if (arg.getKind() == ExpressionTree.Kind.STRING_LITERAL) {
                         String stmt = (String) ((LiteralTree) arg).getValue();
+
+                        // get result type and placeholder type of prepared statement
+                        String[] in = getInType(stmt);
                         String[] out = getOutType(stmt);
                         if (out == null) {
                             checker.reportWarning(
                                     tree, "could not get result type of prepared statement");
-                        } else {
-                            type.replaceAnnotation(createSQLAnnotation(out));
                         }
+                        if (in == null) {
+                            checker.reportWarning(
+                                    tree, "could not get placeholder type of prepared statement");
+                        }
+
+                        type.replaceAnnotation(createSQLAnnotation(in, out));
                     }
                 }
             }
@@ -195,8 +275,28 @@ public class OpsAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
             return super.visitMethodInvocation(tree, type);
         }
 
+        /** Returns a new SQL annotation with the given output type. */
+        private AnnotationMirror createSQLAnnotation(String[] in, String[] out) {
+            AnnotationBuilder builder = new AnnotationBuilder(processingEnv, Sql.class);
+            if (in != null) builder.setValue("in", in);
+            if (out != null) builder.setValue("out", out);
+            return builder.build();
+        }
+
         private String[] getOutType(String stmt) {
-            return schemaInfo.getResultTypeOf(stmt).toArray(new String[0]);
+            try {
+                return schemaInfo.getResultTypeOf(stmt).toArray(new String[0]);
+            } catch (OpsDatabaseException e) {
+                return null;
+            }
+        }
+
+        private String[] getInType(String stmt) {
+            try {
+                return schemaInfo.getPlaceholderTypesOf(stmt).toArray(new String[0]);
+            } catch (OpsDatabaseException e) {
+                return null;
+            }
         }
     }
 }
