619e2922b91803a10a2b35581cf0bf0feba0b77d
Thomas Kirz <thomas.kirz@gmail.com>
2024-02-27 17:00:09 +0100

    add JavaDoc

--- src/main/java/io/github/eisop/opsc/OpsStats.java
+++ src/main/java/io/github/eisop/opsc/OpsStats.java
@@ -1,23 +1,39 @@
 package io.github.eisop.opsc;
 
+/**
+ * The OpsStats class is used to keep track of the number of prepared statements found in the
+ * statement.
+ */
 public class OpsStats {
 
     private int numAnnotatedPreparedStatements = 0;
 
     private int numUnsupportedPreparedStatements = 0;
 
+    /** Increments the count of annotated prepared statements. */
     public void recordAnnotatedPreparedStatement() {
         numAnnotatedPreparedStatements++;
     }
 
+    /** Increments the count of unsupported or invalid prepared statements. */
     public void recordUnsupportedPreparedStatement() {
         numUnsupportedPreparedStatements++;
     }
 
+    /**
+     * This method is used to get the count of annotated prepared statements.
+     *
+     * @return The number of annotated prepared statements.
+     */
     public int getNumAnnotatedPreparedStatements() {
         return numAnnotatedPreparedStatements;
     }
 
+    /**
+     * This method is used to get the count of unsupported or invalid prepared statements.
+     *
+     * @return The number of unsupported prepared statements.
+     */
     public int getNumUnsupportedPreparedStatements() {
         return numUnsupportedPreparedStatements;
     }
