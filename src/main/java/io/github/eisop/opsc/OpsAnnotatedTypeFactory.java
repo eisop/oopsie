6b061e8b3384cd13bcaf8edfd69c0a36da047005
Thomas Kirz <thomas.kirz@gmail.com>
2024-07-31 16:50:06 +0200

    don't analyse queries twice

--- src/main/java/io/github/eisop/opsc/OpsAnnotatedTypeFactory.java
+++ src/main/java/io/github/eisop/opsc/OpsAnnotatedTypeFactory.java
@@ -394,7 +394,8 @@ public class OpsAnnotatedTypeFactory extends BaseAnnotatedTypeFactory {
                     if (stmt != null) {
                         AnnotationMirror annotation = buildSqlAnnotation(stmt, tree);
                         if (annotation != null) {
-                            type.replaceAnnotation(buildSqlAnnotation(stmt, tree));
+                            type.replaceAnnotation(annotation);
+
                             logger.supportedPreparedStatement(
                                     getRoot(),
                                     trees.getSourcePositions().getStartPosition(getRoot(), tree));
